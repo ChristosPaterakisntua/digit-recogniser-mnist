86291d1beb56dcea98b242ce5b4c2913be8fd702
ChristosPaterakisntua <christospaterakis1@gmail.com>
2026-08-14 10:36:05 +0300

    initial commit: dataset loading and inspection & raw image loader

---
+++ dataset.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+
+BATCH_SIZE = 5
+
+
+def load_raw_mnist(show_details: bool = False) -> None:
+    train_dataset = datasets.MNIST(
+        root="data",
+        train=True,
+        download=True,
+    )
+    test_dataset = datasets.MNIST(
+        root="data",
+        train=False,
+        download=True,
+    )
+    if show_details:
+        print(f"{len(train_dataset)} train images")  # 60000
+        print(f"{len(test_dataset)} test images")  # 10000
+        image, label = train_dataset[0]
+        print(f"Image type: {type(image)}")  # <class 'PIL.Image.Image'>
+        print(f"Label type: {type(label)}")  # <class 'int'>
+        print(f"pixels: {image.size}")  # (28, 28)
+        plt.figure(figsize=(15, 3))
+        for i in range(BATCH_SIZE):
+            image, label = train_dataset[i]
+            plt.subplot(1, BATCH_SIZE, i + 1)
+            plt.imshow(image, cmap="gray")
+            plt.title(f"Label: {label}")
+            plt.axis("off")
+        plt.gcf().canvas.manager.set_window_title("MNIST Dataset Preview")
+        plt.show()
+
+
+def load_transformed_mnist(show_details: bool = False) -> None:
+    transform = transforms.ToTensor()
+    train_dataset = datasets.MNIST(
+        root="data",
+        train=True,
+        transform=transform,
+        download=True,
+    )
+    test_dataset = datasets.MNIST(
+        root="data",
+        train=False,
+        transform=transform,
+        download=True,
+    )
+    if show_details:
+        image, label = train_dataset[0]
+        print(f"Image type: {type(image)}")  # <class 'torch.Tensor'>
+        print(f"Label type: {type(label)}")  # <class 'int'>
+        print(f"Tensor shape: {image.shape}")  # torch.Size([1, 28, 28])
+
+
+def load_mnist(
+    batch_size: int = BATCH_SIZE,
+    train: bool = True,
+) -> DataLoader:
+    """
+    Loads a batch of images from the dataset
+
+    Args
+    ------
+    batch_size : int
+        the number of images loaded in this batch. Default: :data:`BATCH_SIZE`
+    train : bool
+        indicates wether to use train or test dataset. Default: `True`
+
+    Returns
+    -------
+    Dataloader
+        Dataloader's data tensor shape: [batch_size, 1, 28, 28]
+    """
+    transform = transforms.ToTensor()
+    dataset = datasets.MNIST(
+        root="data",
+        train=train,
+        transform=transform,
+        download=True,
+    )
+    return DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+
+if __name__ == "__main__":
+    dataset = load_mnist()
+    images, labels = next(iter(dataset))
+    print(f"Full image tensor shape: {images.shape}")  # torch.Size([5, 1, 28, 28])
+    print(f"Label tensor shape: {labels.shape}")  # torch.Size([5])
+    print(images[0]) # the grayscale image's colors are normalized (mapped to 0.0 - 1.0 where 0.0 = white, 1.0 = black)
---
+++ loader.py
@@ -0,0 +1,62 @@
+from PIL import Image, ImageOps
+import torch
+from torchvision import transforms 
+
+
+def normalize_tensor(tensor : torch.Tensor) -> torch.Tensor:
+    """
+    All values are normalized to 0.0 - 1.0 using z score method
+
+    Args
+    ------
+    tensor : torch.Tensor
+        the input tensor 
+    
+    Returns
+    ------
+    torch.Tensor 
+        normalized tensor
+
+    Note
+    ------
+    `y = (x - mean) / std` where:
+    - x: input tensor
+    - y: output tensor
+    """
+    tensor = tensor.to(dtype=torch.float32)
+    mean, std = torch.mean(tensor), torch.std(tensor)
+    tensor = (tensor - mean) / std
+    return tensor.to(dtype=torch.float32)
+
+
+def load_raw_image(path : str) -> torch.Tensor:
+    """
+    Loads a raw image and returns a Tensor of shape [1,28,28]
+    ([channels, height, width]). The image is converted to grayscale and 
+    resized to 28x28 px. The purpose of this function is to produce tensors
+    that match mnist dataset tensors.
+    """
+    path = path.strip().strip('"')
+    with Image.open(path) as img:
+        img = ImageOps.grayscale(img)
+        img = ImageOps.autocontrast(img)
+        img = img.resize(size=(28,28))
+        # remove this
+        img.show()
+
+        transform = transforms.ToTensor()
+        img_tensor = transform(img)
+    print(img_tensor.dtype)
+    print(img_tensor.shape)
+    print(img_tensor.min())
+    print(img_tensor.max())
+    return img_tensor
+
+
+def main():
+    path = input("Path: ")
+    print(load_raw_image(path))
+
+
+if __name__ == "__main__":
+    main()
